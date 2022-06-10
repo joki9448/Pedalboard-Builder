ab2de37c18008b0d9483b3037eb002f27097d38b
John Kim <joki9448@gmail.com>
2022-06-10 11:15:21 -0400

    set up some more controller methods

--- app/controllers/configs_controller.rb
+++ app/controllers/configs_controller.rb
@@ -1,2 +1,6 @@
 class ConfigsController < ApplicationController
+
+    def index
+        render json: Config.all, status: 200
+    end
 end
--- app/controllers/pedals_controller.rb
+++ app/controllers/pedals_controller.rb
@@ -1,5 +1,10 @@
 class PedalsController < ApplicationController
     def index
-        render json: Pedal.all, status:200
+        render json: Pedal.all, status: 200
+    end
+
+    def show
+        pedal = Pedal.find_by(id: params[:id])
+        render json: pedal, status: 200
     end
 end
