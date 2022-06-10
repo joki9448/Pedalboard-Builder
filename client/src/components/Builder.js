69e83aa86d8a150646fc7cf2e22ff1f60bdcc194
John Kim <joki9448@gmail.com>
2022-06-10 12:36:10 -0400

    created menu component

--- client/src/components/Builder.js
+++ client/src/components/Builder.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import Pedals from './Pedals';
+import Menu from './Menu';
 
 function Builder() {
 
@@ -10,7 +11,7 @@ function Builder() {
             </Link>
             <h1>Build Your Board</h1>
             <div className="menu-container">
-
+                <Menu />
             </div>
         </div>
     )
