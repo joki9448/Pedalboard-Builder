78c1bc3b6dd89efc231e56795bd9cf90d8cbb7a2
John Kim <joki9448@gmail.com>
2022-06-10 12:25:22 -0400

    more routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Builder from './components/Builder';
+import Collection from './components/Collection';
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
           </Route>
           <Route exact path="/builder">
             <Builder />
+          </Route>
+          <Route exact path="/collection">
+            <Collection />
           </Route>
         </Switch>
       </Router>
--- client/src/components/Collection.js
+++ client/src/components/Collection.js
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
 
-function Builder() {
+function Collection() {
     return (
         <div>
             <Link to="/">
                 <button>HOME</button>
             </Link>
-            <h1>Hello World</h1>
+            <h1>Your Boards</h1>
         </div>
     )
 }
 
-export default Builder;
+export default Collection;
