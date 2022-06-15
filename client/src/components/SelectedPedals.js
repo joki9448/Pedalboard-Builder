f60fe3aa0333822c3547f6c134d2c11764c276b0
John Kim <joki9448@gmail.com>
2022-06-15 15:17:28 -0400

    trying to get pedals to not go outside of pb container

--- client/src/components/SelectedPedals.js
+++ client/src/components/SelectedPedals.js
@@ -1,6 +1,5 @@
 import {useState} from 'react'
 
-
 function SelectedPedals({ p }){
     const { id, brand, model, effect } = p
     const [isDragging, setIsDragging] = useState(false)
@@ -10,29 +9,6 @@ function SelectedPedals({ p }){
       e.dataTransfer.setData("model", model);
     }
 
-
-    // const handleDragOver = (e) => {
-    //     e.preventDefault()
-    //     let model = e.dataTransfer.getData("model")
-    //     let id = e.dataTransfer.getData("id")
-    //     console.log('Dragged item V', model, id)
-    //     let foundPedal = p.all.find((ped) => ped.id == id)
-        
-    //     setPedals((prevState) => {
-    //         // console.log('dropped pedal objs', prevState.dropped)
-
-    //         let droppedIds = []
-    //         let stopDuplicates = prevState.dropped.filter((p) => {
-    //             droppedIds.push(p.id)
-    //             return droppedIds
-    //             // console.log('foundPedal obj in stopduplicates', foundPedal.id)
-    //         })
-    //         console.log('dropped ids', droppedIds)
-    //         if (!droppedIds.includes(foundPedal.id))
-    //         return  {...prevState, ['dropped']: [...prevState.dropped, foundPedal]}
-    //         // else
-    //         //     return null
-    // })
 
     return (
         <div>
