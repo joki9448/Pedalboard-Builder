a1b348d1443b8f39254c3ffdcd3c38689fc1c884
John Kim <joki9448@gmail.com>
2022-06-16 10:40:53 -0400

    started styling homepage

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -3,15 +3,20 @@ import { Link } from 'react-router-dom';
 function Navigation() {
     
     return (
-        <div>
+        <div className="home-container">
+            <h1>Pedalboard Builder</h1>
             <nav className="nav">
-                <h1>Pedalboard Builder</h1>
-                <Link to="/builder">
-                    <img className="nav-img" alt="builder" src="/images/tobuilderimage.jpeg" height="750"/>
-                </Link>
-                <Link to="/collection">
-                    <img className="nav-img" alt="collection" src="/images/toboardcollection.jpeg" height="750" width="1000"/>
-                </Link>
+                    <Link to="/builder">
+                        <div className="nav-img-container-1">
+                            <img className="nav-img-1" alt="builder" src="/images/tobuilderimage.jpeg"/>
+                        </div>
+                    </Link>
+                    <Link to="/collection">
+                        <div className="nav-img-container-2">
+                            <img className="nav-img-2" alt="collection" src="/images/toboardcollection.jpeg"/>
+                        </div>
+                    </Link>
+                        {/* <div className="content"> Hello World </div>  */}
             </nav>
         </div>
     )
