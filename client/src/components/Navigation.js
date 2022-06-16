6ab91f2e00457dbbc77a5f3c3093b0be19231e25
John Kim <joki9448@gmail.com>
2022-06-16 15:09:03 -0400

    adjusted header on nav page

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -4,7 +4,7 @@ function Navigation() {
     
     return (
         <div className="home-container">
-            {/* <h1>Pedalboard Builder</h1> */}
+            <h1>Pedalboard Builder</h1>
             <nav className="nav">
                     <Link to="/builder">
                         <div className="nav-img-container-1">
@@ -13,10 +13,12 @@ function Navigation() {
                     </Link>
                     <Link to="/collection">
                         <div className="nav-img-container-2">
+                            <p className="nav-text">
+                                Your Collection
+                            </p>
                             <img className="nav-img-2" alt="collection" src="/images/toboardcollection.jpeg"/>
                         </div>
                     </Link>
-                        {/* <div className="content"> Hello World </div>  */}
             </nav>
         </div>
     )
