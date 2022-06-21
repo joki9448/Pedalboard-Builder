e4ab7e197ff46776d371568110fa69cea76ad177
John Kim <joki9448@gmail.com>
2022-06-20 23:04:26 -0400

    error changed from not found to unprocessable

--- app/controllers/configs_controller.rb
+++ app/controllers/configs_controller.rb
@@ -3,4 +3,15 @@ class ConfigsController < ApplicationController
     def index
         render json: Config.all, status: 200
     end
+
+    def create 
+        config = Config.create(config_params)
+        render json: config, status: 201
+    end
+
+    private
+
+    def config_params
+        params.permit(:brand, :model, :effect, :user_id)
+    end
 end
