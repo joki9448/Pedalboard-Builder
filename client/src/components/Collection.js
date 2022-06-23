93992fbb40ed169c4c970ee2c0ce15c7588a45b9
John Kim <joki9448@gmail.com>
2022-06-23 04:27:26 -0400

    reimplementing hover over collections for text

--- client/src/components/Collection.js
+++ client/src/components/Collection.js
@@ -35,6 +35,8 @@ function Collection() {
                     return <ConfigContainer 
                     key={e.id} 
                     config={e}
+                    isTextVisible={isTextVisible}
+                    setIsTextVisible={setIsTextVisible}
                     />
                 })}
             </div>
--- client/src/components/ConfigContainer.js
+++ client/src/components/ConfigContainer.js
@@ -1,14 +1,23 @@
 function ConfigContainer({ config, isTextVisible, setIsTextVisible }) {
     console.log('config', config)
     const { brand, model, effect } = config
+
+    const handleMouseEnter = () => {
+        setIsTextVisible(true)
+    }
+    const handleMouseLeave = () => {
+        setIsTextVisible(false)
+    }
     return (
         <div>
             <img
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
                 className="config-item"
                 src={`/images/${brand.replace(' ', '-').toLowerCase()}/${effect.replace(' / ', '-').toLowerCase()}/${model.replace(' ', '-').toLowerCase()}.jpeg`}
             />
-            <div className="model-text">{model}</div>
-            <div className="effect-text">{effect}</div>
+            <div className="model-text">{isTextVisible ? model : null}</div>
+            <div className="effect-text">{isTextVisible ? effect : null}</div>
         </div>
     )
 }
